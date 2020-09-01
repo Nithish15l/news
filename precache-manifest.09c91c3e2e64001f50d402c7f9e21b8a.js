11e70766fdbc94e85fe567ea7cc68ea57e114fd3
Nithish77 <lemburunithish7@gmail.com>
2020-09-01 11:58:11 +0530

    Updates

---
+++ precache-manifest.09c91c3e2e64001f50d402c7f9e21b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838e36dc92f9fec8dd661dff60df10d9",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "b82a1dce28805df7bb4e",
+    "url": "/news/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "4c1ef29369e81af8655f",
+    "url": "/news/static/js/2.284d16bf.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/news/static/js/2.284d16bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b82a1dce28805df7bb4e",
+    "url": "/news/static/js/main.90026b74.chunk.js"
+  },
+  {
+    "revision": "97f8f9d4f2a75f0e178a",
+    "url": "/news/static/js/runtime-main.a034b595.js"
+  }
+]);
